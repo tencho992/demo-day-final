4cb848fc6366ea240c2af8979a4877d369ff985e
Tenzin Chosang <tenzinchosang.dev@gmail.com>
2023-05-16 14:59:53 -0400

    works without quizOne

--- public/assets/js/quizOne.js
+++ public/assets/js/quizOne.js
@@ -1,1 +1,10 @@
-alert('itsworking')
+const nextBtn = document.getElementById('next')
+const answerOne = document.getElementById('one').addEventListener('click', answerOneCompare)
+const answerTwo = document.getElementById('two').addEventListener('click', answerTwoCompare)
+const answerThree = document.getElementById('three').addEventListener('click', answerThreeCompare)
+const correctIndex = document.getElementById('index')
+function answerOneCompare(){
+    if(answerOne.value == correctIndex){
+        nextBtn.hidden= false;
+    }
+}
