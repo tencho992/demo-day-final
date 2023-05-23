993a0647a4f81a114b2baf2d8d3841c5ee118055
Tenzin Chosang <tenzinchosang.dev@gmail.com>
2023-05-22 20:13:37 -0400

    config

--- src/server.js
+++ src/server.js
@@ -7,15 +7,15 @@ const MongoStore = require("connect-mongo")(session);
 const methodOverride = require("method-override");
 const flash = require("express-flash");
 const logger = require("morgan");
-const connectDB = require("./config/database");
-const mainRoutes = require("./routes/main");
-const postRoutes = require("./routes/posts");
+const connectDB = require("../config/database");
+const mainRoutes = require("../routes/main");
+const postRoutes = require("../routes/posts");
 
 //Use .env file in config folder
-require("dotenv").config({ path: "./config/.env" });
+require("dotenv").config({ path: "./../config/.env" });
 
 // Passport config
-require("./config/passport")(passport);
+require("../config/passport")(passport);
 
 //Connect To Database
 connectDB();
